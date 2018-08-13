e25e96d0ab9bb1449a4e06e79051c6cd3bc232bd
Fernando Cortez <fernandocortez@users.noreply.github.com>
2018-08-13 16:57:43 -0400

    Refactored code to deploy as a Google Cloud Function

--- index.js
+++ index.js
@@ -1,26 +1,53 @@
 const puppeteer = require('puppeteer');
 
-(async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto('https://thehomedepot.alohaenterprise.com:4430/memberlink/GiftCards.html?companyID=thd01');
+exports.getBalance = async function getBalance(req, res) {
+    if (req.method !== 'POST') {
+        return res.status(405).end();
+    }
+    if (req.get('content-type') !== 'application/json') {
+        return res.status(406).end();
+    }
+    if (!req.body.account_number) {
+        return res.status(400).send('Account number not present in request');
+    }
+    if (!/^\d+$/.test(req.body.account_number)) {
+        return res.status(400).send('Account number is malformed');
+    }
 
-    // Type into search box
-    const cardNumber = process.env.ACCOUNT_NUMBER;
-    await page.type('input.formField', cardNumber);
-    // Click search button
-    await page.click('input#gcCheckCardButton');
+    const cardNumber = String(req.body.account_number);
 
-    const cardBalance = await page.evaluate(() => document.querySelector('#cardBalanceDiv > h2').textContent);
-    const cardHistory = await page.evaluate(() => {
-        const history = Array.prototype.slice.call(document.querySelectorAll('#cardHistory > #table-1 > tbody > tr'));
-        const recentEntries = history.slice(0, 5);
-        return recentEntries.map(({cells}) => {
-            const entryData = Array.prototype.slice.call(cells).map(cell => cell.textContent);
-            const [date, _, amount, balance] = entryData;
-            return {date, amount, balance};
+    try {
+        const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
+        const page = await browser.newPage();
+        await page.goto('https://thehomedepot.alohaenterprise.com:4430/memberlink/GiftCards.html?companyID=thd01');
+
+        // Type into search box
+        await page.type('input.formField', cardNumber);
+        // Click search button
+        await page.click('input#gcCheckCardButton');
+
+        const cardBalance = await page.evaluate(() => {
+            const node = document.querySelector('#cardBalanceDiv > h2');
+            return node === null ? '0.00' : node.textContent;
         });
-    });
+        const cardHistory = await page.evaluate(() => {
+            return Array.prototype.slice.call(document.querySelectorAll('#cardHistory > #table-1 > tbody > tr'))
+                .slice(0, 5)
+                .map(({ cells }) => {
+                    const row = Array.prototype.slice.call(cells).map(cell => cell.textContent);
+                    const [date, _, amount, balance] = row;
+                    return { date, amount, balance };
+                });
+        });
 
-    await browser.close();
-})();
+        await browser.close();
+
+        return res.status(200).set('Content-Type', 'application/json').send({
+            balance: cardBalance,
+            history: cardHistory,
+        });
+    } catch (err) {
+        console.error(err);
+        return res.status(500).end();
+    }
+};
