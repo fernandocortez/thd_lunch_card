8bea2571134ddc476d051d9ed3c3d09b67551a73
Fernando Cortez <fccortez1@hotmail.com>
2020-03-31 22:43:26 -0400

    Adding cloud function to send sms

---
+++ cloud-functions/send-sms/index.js
@@ -0,0 +1,19 @@
+const client = require('twilio')(process.env.ACCOUNT_SID, process.env.AUTH_TOKEN);
+const twilioPhoneNumber = process.env.TWILIO_PHONE_NUMBER;
+
+exports.sendSms = function sendSms(pubSubEvent, context, callback) {
+  let messageProperties;
+  try {
+    messageProperties = JSON.parse(Buffer.from(pubSubEvent.data, 'base64').toString());
+  } catch (err) {
+    return callback(err, 'Failed to parse event data');
+  }
+
+  client.messages
+    .create({
+      ...messageProperties,
+      from: twilioPhoneNumber
+    })
+    .then(message => callback(null, message))
+    .catch(err => callback(err, 'Failed to send SMS'));
+};
